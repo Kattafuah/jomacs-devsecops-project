9dfc8269de7af02812d4dd970acec96d2f27cbc4
kattafuah <kattafuah@gmail.com>
2025-03-05 18:02:56 +0000

    added opa-policies and steps to actions file

--- ci-scripts/opa-validate.sh
+++ ci-scripts/opa-validate.sh
@@ -4,10 +4,10 @@ set -e  # Exit on error
 echo "Validating Kubernetes manifests with OPA..."
 
 # Ensure the log directory exists
-mkdir -p opa-logs
+mkdir -p opa-logs 
 
 # Run OPA validation using all policies together
-opa eval --input eks-manifest-files/app.yaml --data opa-policies/ \
+opa eval --input eks-manifest-files/app.yaml --data ./opa-policies/ \
     '{sports: data.k8sports.violation, root: data.k8root.violation, volumes: data.k8svolumes.violation}' | tee opa-logs/opa-validation.log
 
 # Check if any violations exist
